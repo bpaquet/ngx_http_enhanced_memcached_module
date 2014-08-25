2c7f6126b55d6b9c7470a47dac7e941812262147
Bertrand Paquet <bertrand.paquet@gmail.com>
2014-08-26 00:04:12 +0200

    Automatic nginx build

--- tests/run.sh
+++ tests/run.sh
@@ -5,6 +5,18 @@ echo flush_all | nc localhost 11211 | grep OK > /dev/null
 rm -rf work
 mkdir work
 mkdir work/logs
+if [ "$NGINX_BUILD" != "" ]; then
+	set -e
+	cd work
+	wget http://nginx.org/download/nginx-$NGINX_BUILD.tar.gz
+	tar xvzf nginx-$NGINX_BUILD.tar.gz
+	cd nginx-$NGINX_BUILD
+	./configure --add-module=../../../../ngx_http_enhanced_memcached_module
+	make
+	export NGINX_BIN=$(pwd)/objs/nginx
+	set +e
+	cd ../../
+fi
 cp nginx.conf work
 $NGINX_BIN -p $(pwd)/work -c nginx.conf
 sleep 1
@@ -15,5 +27,7 @@ res=$?
 kill $(cat work/nginx.pid)
 
 sleep 1
+
+rm -rf work
 
 exit $res
