49f39ac2a5462aea413fc597b8fdd4a5270cba31
Bertrand Paquet <bertrand.paquet@gmail.com>
2014-08-30 14:46:28 +0200

    More script improvment

--- tests/run.sh
+++ tests/run.sh
@@ -14,10 +14,13 @@ if [ "$NGINX_BUILD" != "" ]; then
 	cd work
 	echo "Building nginx $NGINX_BUILD"
 	wget http://nginx.org/download/nginx-$NGINX_BUILD.tar.gz
-	tar xvzf nginx-$NGINX_BUILD.tar.gz
+	tar xzf nginx-$NGINX_BUILD.tar.gz
 	cd nginx-$NGINX_BUILD
+	echo "Configuring ..."
 	./configure --with-debug --add-module=../../../../ngx_http_enhanced_memcached_module > build.log
+	echo "Building ..."
 	make > build.log
+	echo "Build OK."
 	export NGINX_BIN=$(pwd)/objs/nginx
 	set +e
 	cd ../../
@@ -34,8 +37,8 @@ kill $(cat work/nginx.pid)
 sleep 1
 
 if [ $res != 0 ]; then
-	curl -s -X POST ec2-54-76-187-89.eu-west-1.compute.amazonaws.com:1337 --data-binary @work/logs/error.log -H 'Content-type: application/octet-stream'
-	curl -s -X POST ec2-54-76-187-89.eu-west-1.compute.amazonaws.com:1337 --data-binary @work/logs/access.log -H 'Content-type: application/octet-stream'
+	# curl -s -X POST ec2-54-76-187-89.eu-west-1.compute.amazonaws.com:1337 --data-binary @work/logs/error.log -H 'Content-type: application/octet-stream'
+	# curl -s -X POST ec2-54-76-187-89.eu-west-1.compute.amazonaws.com:1337 --data-binary @work/logs/access.log -H 'Content-type: application/octet-stream'
 fi
 
 rm -rf work
